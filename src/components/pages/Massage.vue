124671d2eb5e372c6f6aba8dc9c4bb4c9f91c2e3
coffeeteer <tgrieshiemer@gmail.com>
2020-03-25 22:34:03 -0400

    added VueRouter setup

---
+++ src/components/pages/Massage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>Massage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import { routes } from './routes.js'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
 
 Vue.config.productionTip = false
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
