9f32f53e16ab3bcccec24574dc332c41685edcc3
coffeeteer <tgrieshiemer@gmail.com>
2020-03-27 23:01:10 -0400

    added the bootstrap Header, still needs more sytling and adjustments

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        <h1>Header </h1>
-    </div>    
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <!-- <svg><use href="/../assets/jackie-beecher-card-flower.svg"></svg> -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo02"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="nav nav-pills mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" li="tag" to="/" exact><a>Home</a></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" li="tag" to="/massage"><a>Massage</a></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" li="tag" to="/yoga"><a>Yoga</a></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" li="tag" to="/acupuncture"><a>Acupuncture</a></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" li="tag" to="/contact"><a>Contact Us</a></router-link>
+          </li>
+        </ul>
+        
+      </div>
+    </nav>
+  </div>
 </template>
